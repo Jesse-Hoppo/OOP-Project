3f3268518e966effde107e63b35f8b26820da39b
Jesse Hoppo <jesse.hoppo11@gmail.com>
2023-05-05 03:17:57 +0930

    Added basic Employee Class. Will be tested to run in main for printing

--- payslip.h
+++ payslip.h
@@ -6,11 +6,21 @@ using namespace std;
 #ifndef PAYSLIP_H
 #define PAYSLIP_H
 
+class Employee {
+    protected:
+        string name;
+        string location;
+        string job_postion;
+        int YOE;
+        
+    public:
+    Employee( string s_name, string s_location, string s_job_postion, int s_YOE) {
+        name = s_name;
+        location = s_location;
+        job_postion = s_job_postion;
+        YOE = s_YOE;
 
+    }
 
-
-
-
+};
 #endif
-
-
